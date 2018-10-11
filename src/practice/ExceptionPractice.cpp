4631d3a5db6416561523526b1bf529c072b2f84b
Zsago <3427241441@qq.com>
2018-10-11 10:18:29 +0800

    将对象用作异常类型

--- src/practice/ExceptionPractice.cpp
+++ src/practice/ExceptionPractice.cpp
@@ -8,6 +8,7 @@
 #include "ExceptionPractice.h"
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
 
 ExceptionPractice::ExceptionPractice() {
 	// TODO Auto-generated constructor stub
@@ -89,6 +90,38 @@ void ExceptionPractice::exception_try_catch() {
 	cout << "Bye!" << endl;
 }
 
+void ExceptionPractice::exception_try_catch_by_object() {
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	double hmean_throw_bad_hmean(double a, double b);
+	double gmean_throw_bad_gmean(double a, double b);
+
+	double x, y, z;
+	cout << "========== exception_try_catch_by_object ==========" << endl
+			<< "Enter two numbers: ";
+	while (cin >> x >> y) {
+		try {
+			z = hmean_throw_bad_hmean(x, y);
+			cout << "Harmonic mean of " << x << " and " << y << " is " << z
+					<< endl;
+			cout << "Geometric mean of " << x << " and " << y << " is "
+					<< gmean_throw_bad_gmean(x, y) << endl;
+			cout << "Enter next set of numbers <q to quit>: ";
+		} catch (bad_hmean & bg) {
+			bg.mesg();
+			cout << "Try again." << endl;
+			continue;
+		} catch (bad_gmean & hg) {
+			cout << hg.mesg();
+			cout << "Values used: " << hg.v1 << ", " << hg.v2 << endl;
+			cout << "Sorry, you don't get to play any more." << endl;
+			break;
+		}
+	}
+	cout << "Bye!" << endl;
+}
+
 double hmean(double a, double b) {
 	using std::cout;
 	using std::endl;
@@ -120,8 +153,24 @@ double hmean_throw(double a, double b) {
 	return 2.0 * a * b / (a + b);
 }
 
+double hmean_throw_bad_hmean(double a, double b) throw (bad_hmean) {
+	using std::cout;
+	using std::endl;
+	if (a == -b) {
+		throw bad_hmean(a, b);
+	}
+	return 2.0 * a * b / (a + b);
+}
+
+double gmean_throw_bad_gmean(double a, double b) throw (bad_gmean) {
+	if (a < 0 || b < 0)
+		throw bad_gmean(a, b);
+	return sqrt(a * b);
+}
+
 void ExceptionPractice::main() const {
 	ExceptionPractice ep;
+	ep.exception_try_catch_by_object();
 	ep.exception_try_catch();
 	ep.exception_code();
 	ep.exception_abort();
--- src/practice/ExceptionPractice.h
+++ src/practice/ExceptionPractice.h
@@ -16,7 +16,38 @@ public:
 	void exception_abort();
 	void exception_code();
 	void exception_try_catch();
+	void exception_try_catch_by_object();
 	void main() const;
 };
+
+class bad_hmean {
+private:
+	double v1;
+	double v2;
+public:
+	bad_hmean(double a = 0, double b = 0) :
+			v1(a), v2(b) {
+	}
+	void mesg();
+};
+
+inline void bad_hmean::mesg() {
+	std::cout << "hmean(" << v1 << ", " << v2 << "): "
+			<< "invalid arguments: a = -b" << std::endl;
+}
+
+class bad_gmean {
+public:
+	double v1;
+	double v2;
+	bad_gmean(double a = 0, double b = 0) :
+			v1(a), v2(b) {
+	}
+	const char * mesg();
+};
+
+inline const char * bad_gmean::mesg() {
+	return "gmean() arguments should be >= 0\n";
+}
 
 #endif /* PRACTICE_EXCEPTIONPRACTICE_H_ */
