1b36d80106ed7925f865e35e49c7811672200412
Zsago <3427241441@qq.com>
2018-11-05 10:15:24 +0800

    cout.setf多种用法

--- src/practice/CoutPractice.cpp
+++ src/practice/CoutPractice.cpp
@@ -8,6 +8,7 @@
 #include "CoutPractice.h"
 #include<iostream>
 #include<string.h>
+#include <math.h>
 using std::cout;
 using std::endl;
 using std::ios_base;
@@ -98,6 +99,43 @@ void CoutPractice::print_setf() const {
 	cout << "How " << true << "! oops - How ";
 	cout.setf(ios_base::boolalpha);
 	cout << true << "!\n";
+	cout << "----------" << endl;
+
+	cout.fill(' ');
+	cout.unsetf(ios_base::boolalpha);
+	cout.setf(ios_base::dec, ios_base::basefield);
+	cout.setf(ios_base::left, ios_base::adjustfield);
+	cout.setf(ios_base::showpos);
+	cout.setf(ios_base::showpoint);
+	cout.precision(3);
+	ios_base::fmtflags old = cout.setf(ios_base::scientific,
+			ios_base::floatfield);
+	cout << "Left Justification: \n";
+	long n;
+	for (n = 1; n <= 41; n += 10) {
+		cout.width(4);
+		cout << n << "|";
+		cout.width(12);
+		cout << sqrt(double(n)) << "|\n";
+	}
+	cout.setf(ios_base::internal, ios_base::adjustfield);
+	cout.setf(old, ios_base::floatfield);
+	cout << "Internal Justification: \n";
+	for (n = 1; n <= 41; n += 10) {
+		cout.width(4);
+		cout << n << "|";
+		cout.width(12);
+		cout << sqrt(double(n)) << "|\n";
+	}
+	cout.setf(ios_base::right, ios_base::adjustfield);
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	cout << "Right Justification: \n";
+	for (n = 1; n <= 41; n += 10) {
+		cout.width(4);
+		cout << n << "|";
+		cout.width(12);
+		cout << sqrt(double(n)) << "|\n";
+	}
 }
 
 void CoutPractice::print_format() const {
