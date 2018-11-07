f1d7a50ad0bba0d29bdd6ce4b3f470213166e6e7
Zsago <3427241441@qq.com>
2018-11-07 10:05:02 +0800

    流状态的影响

--- src/practice/CinPractice.cpp
+++ src/practice/CinPractice.cpp
@@ -8,6 +8,7 @@
 #include "CinPractice.h"
 #include <iostream>
 #include <exception>
+#include <stdlib.h>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -19,7 +20,7 @@ CinPractice::CinPractice() {
 }
 
 void CinPractice::main() const {
-	cin.exceptions(ios_base::failbit);
+//	cin.exceptions(ios_base::failbit);
 	cout << "Enter numbers: ";
 	int sum = 0;
 	int input;
@@ -27,12 +28,26 @@ void CinPractice::main() const {
 		while (cin >> input) {
 			sum += input;
 		}
+		if (cin.eof()) {
+			cout << "Loop terminated because EOF encountered\n";
+		}
 	} catch (ios_base::failure & bf) {
 		cout << bf.what() << endl;
 		cout << "0! the horror!\n";
 	}
 	cout << "Last value entered = " << input << endl;
 	cout << "Sum = " << sum << endl;
+	if (cin.fail() && !cin.eof()) {
+		cin.clear();
+		while (!isspace(cin.get())) {
+			continue;
+		}
+	} else {
+		cout << "I cannot go on!\n";
+		exit(1);
+	}
+	cout << "Now enter a new number: ";
+	cin >> input;
 }
 
 CinPractice::~CinPractice() {
