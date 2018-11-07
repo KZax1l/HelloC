e6eadf0ecb128a0061d55849fd74f24787ea54e4
Zsago <3427241441@qq.com>
2018-11-07 09:49:47 +0800

    I/0和异常

--- src/practice/CinPractice.cpp
+++ src/practice/CinPractice.cpp
@@ -7,9 +7,11 @@
 
 #include "CinPractice.h"
 #include <iostream>
+#include <exception>
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ios_base;
 
 CinPractice::CinPractice() {
 	// TODO Auto-generated constructor stub
@@ -17,11 +19,17 @@ CinPractice::CinPractice() {
 }
 
 void CinPractice::main() const {
+	cin.exceptions(ios_base::failbit);
 	cout << "Enter numbers: ";
 	int sum = 0;
 	int input;
-	while (cin >> input) {
-		sum += input;
+	try {
+		while (cin >> input) {
+			sum += input;
+		}
+	} catch (ios_base::failure & bf) {
+		cout << bf.what() << endl;
+		cout << "0! the horror!\n";
 	}
 	cout << "Last value entered = " << input << endl;
 	cout << "Sum = " << sum << endl;
